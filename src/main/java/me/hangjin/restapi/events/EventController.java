9fcdf8da83f820e0ad325ff6406e9635061f09e5
hangs0908 <hangs0908@gmail.com>
2021-02-22 11:47:07 +0900

    입력값 전달하면 json 응답으로 201이 나오는지 확인

--- src/main/java/me/hangjin/restapi/events/EventController.java
+++ src/main/java/me/hangjin/restapi/events/EventController.java
@@ -1,8 +1,10 @@
 package me.hangjin.restapi.events;
 
+import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -11,13 +13,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
 
-    @PostMapping("/api/events")
+    @PostMapping
     public ResponseEntity createEvent(@RequestBody Event event) {
-        URI newUri = linkTo(methodOn(EventController.class).createEvent(event)).slash(event.getId()).toUri();
-        return ResponseEntity.created(newUri).build();
+        URI newUri = linkTo(EventController.class).slash("{id}").toUri();
+        event.setId(10);
+        return ResponseEntity.created(newUri).body(event);
     }
 
 
--- src/test/java/me/hangjin/restapi/events/EventControllerTest.java
+++ src/test/java/me/hangjin/restapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package me.hangjin.restapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,8 +9,13 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -19,19 +25,34 @@ class EventControllerTest {
     @Autowired
     MockMvc mockMvc; //mocking이 되어있는 dispacherServlet
 
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     public void createEvent() throws Exception {
         //given
-
-
+        Event event = Event.builder()
+                .name("Spring")
+                .description("Rest api with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,1,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,1,21))
+                .beginEventDateTime(LocalDateTime.of(2018,11,25,1,21))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,1,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitEnrollment(100)
+                .location("강남역 2번 출구 카")
+                .build();
         //when
 
 
         //then
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaTypes.HAL_JSON))
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(jsonPath("id").exists())
                 .andExpect(status().isCreated());
     }
 
