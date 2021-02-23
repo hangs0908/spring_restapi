a220fc3360016c869a5c2ed704acef6fa0adb434
hangs0908 <hangs0908@gmail.com>
2021-02-23 14:04:07 +0900

    Free Offline 에 대한 비즈니스 로직 추가

--- src/test/java/me/hangjin/restapi/events/EventsTest.java
+++ src/test/java/me/hangjin/restapi/events/EventsTest.java
@@ -35,5 +35,61 @@ class EventsTest {
         assertThat(event.getDescription()).isEqualTo(description);
     }
 
+    @Test
+    public void testFree() throws Exception {
+        //given 
+        Event event = Event.builder()
+                .basePrice(0)
+                .maxPrice(0)
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isFree()).isTrue();
+
+        event = Event.builder()
+                .basePrice(100)
+                .maxPrice(0)
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isFree()).isFalse();
+
+        event = Event.builder()
+                .basePrice(0)
+                .maxPrice(100)
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isFree()).isFalse();
+    }
+
+
+    @Test
+    public void testOffline() throws Exception {
+        //given 
+        Event event = Event.builder()
+                .location("네이버 D2 팩토리")
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isTrue();
+
+        event = Event.builder()
+                .build();
+        //when
+        event.update();
+
+        //then
+        assertThat(event.isOffline()).isFalse();
+    }
+
 
 }
